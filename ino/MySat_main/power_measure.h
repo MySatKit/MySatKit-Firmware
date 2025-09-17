f9d75de9d6b672890b7d8e4872401418528c18eb
Timofii K. <kyrpenko.iht@gmail.com>
2025-09-17 18:27:11 +0300

    provide true current values

--- ino/MySat_main/power_measure.h
+++ ino/MySat_main/power_measure.h
@@ -18,6 +18,7 @@ struct ina_struct {
 
 bool initINA() {
   ina.begin();
+  ina.setShuntRes(100, 100, 100);
   return true;
 }
 
@@ -27,12 +28,12 @@ bool initINA() {
 
 ina_struct* get_ina_data() {
   ina_data.batteryVoltage = ina.getVoltage(INA3221_CH1);
-  ina_data.batteryCurrent = ina.getCurrent(INA3221_CH1);
+  ina_data.batteryCurrent = ina.getCurrent(INA3221_CH1) * 1000;
 
   ina_data.SolarPanelVoltage  = (ina.getVoltage(INA3221_CH2) + ina.getVoltage(INA3221_CH3)) / 2;
 
-  ina_data.leftSolarPanelCurrent = ina.getCurrent(INA3221_CH2);
-  ina_data.rightSolarPanelCurrent = ina.getCurrent(INA3221_CH3);
+  ina_data.leftSolarPanelCurrent = ina.getCurrent(INA3221_CH2) * 1000;
+  ina_data.rightSolarPanelCurrent = ina.getCurrent(INA3221_CH3) * 1000;
 
   /*if (ina.getCurrent(INA3221_CH2) +  ina.getCurrent(INA3221_CH3) > 0.01) {
     ina_data.leftSolarPanelRelCurrent = ina.getCurrent(INA3221_CH2) / (ina.getCurrent(INA3221_CH2) + ina.getCurrent(INA3221_CH3)) * 100;
@@ -58,44 +59,10 @@ void print_data(ina_struct* data_) {
   Serial.print(data_->batteryCurrent, 2); 
   Serial.println(" mA");
 
-  //if (data_->SolarPanelVoltage > 0.03 ) {
-    Serial.print("  Solar Panels: U = "); 
-    Serial.print(data_->SolarPanelVoltage, 2); Serial.print(" V,");
+  Serial.print("  Solar Panels: U = "); 
+  Serial.print(data_->SolarPanelVoltage, 2); Serial.print(" V,");
 
-    Serial.println(  RAW DATA:);
-    Serial.print("IL = "); Serial.print(IL); Serial.print("   IR = "); Serial.println(IR);
+  Serial.println("  Current:");
+  Serial.print("  IL = "); Serial.print(IL, 2); Serial.print("mA\n  IR = "); Serial.print(IR, 2); Serial.println(" mA");
 
-    if (IL < 0) IL = 0;
-    if (IR < 0) IR = 0;
-    
-    if (IL + IR > 0.01) {
-      relIL = IL / (IL+IR);
-      relIR = IR / (IL+IR);
-    }
-    else {
-      relIL = 0;
-      relIL = 0;
-    }
-
-    Serial.println(  PROCESSED:);
-
-    Serial.print("  I(L / R) = "); 
-    Serial.print(IL); Serial.print("% / "); Serial.print(IR); Serial.println("%");
-
-    //Serial.print(data_->leftSolarPanelRelCurrent, 0); Serial.print("% / "); 
-    //Serial.print(data_->rightSolarPanelRelCurrent, 0); Serial.println("%");
-
-    Serial.println();
-
-    Serial.println(  REAL DATA:);
-
-
-  //}
-  //else {
-  //  Serial.println("▲ No Solar panels conncted"); 
-  //}
-  
-
-  //DEBUG:
-  Serial.print("I L = "); Serial.print(ina.getCurrent(INA3221_CH2)); Serial.print("  I R = "); Serial.println(ina.getCurrent(INA3221_CH3));
 }
